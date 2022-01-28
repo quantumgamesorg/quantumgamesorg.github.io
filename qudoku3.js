c95bffd7e646c40c33c711ac6b8e0a0f7e32da90
David M <PieKing1215@users.noreply.github.com>
2022-01-28 04:37:32 -0500

    Implement solve function for qudoku3 + placeholder buttons

--- qudoku3.js
+++ qudoku3.js
@@ -10,9 +10,15 @@ boardContainer.insertBefore(makeBoard(100), boardContainer.firstChild);
 
 document.querySelectorAll("#upperLeft button")[0].onclick = () => reset();
 document.querySelectorAll("#upperLeft button")[1].onclick = () => solve();
-document.querySelectorAll("#upperLeft button")[2].onclick = () => document.querySelector("#win_about").classList.toggle("hidden");
-document.querySelectorAll("#upperLeft button")[3].onclick = () => document.querySelector("#win_rules").classList.toggle("hidden");
-document.querySelectorAll("#upperLeft button")[4].onclick = () => window.scrollTo({top: document.querySelectorAll("#content > hr")[0].getBoundingClientRect().top + window.pageYOffset - 40, behavior: "smooth"});
+// TODO: these extra solve buttons are temporary
+document.querySelectorAll("#upperLeft button")[2].onclick = () => solve("26-13");
+document.querySelectorAll("#upperLeft button")[3].onclick = () => solve("30-15");
+document.querySelectorAll("#upperLeft button")[4].onclick = () => solve("32-17");
+document.querySelectorAll("#upperLeft button")[5].onclick = () => solve("54-27");
+
+document.querySelectorAll("#upperLeft button")[6].onclick = () => document.querySelector("#win_about").classList.toggle("hidden");
+document.querySelectorAll("#upperLeft button")[7].onclick = () => document.querySelector("#win_rules").classList.toggle("hidden");
+document.querySelectorAll("#upperLeft button")[8].onclick = () => window.scrollTo({top: document.querySelectorAll("#content > hr")[0].getBoundingClientRect().top + window.pageYOffset - 40, behavior: "smooth"});
 // document.querySelectorAll("#upperLeft button")[4].onclick = () => window.scrollTo({top: document.querySelectorAll("#content > h2")[1].getBoundingClientRect().top + window.pageYOffset - 40, behavior: "smooth"});
 // document.querySelectorAll("#upperLeft button")[5].onclick = () => window.scrollTo({top: document.querySelectorAll("#content > h2")[2].getBoundingClientRect().top + window.pageYOffset - 40, behavior: "smooth"});
 // document.querySelectorAll("#upperLeft button")[6].onclick = () => window.scrollTo({top: document.querySelectorAll("#content > h2")[3].getBoundingClientRect().top + window.pageYOffset - 40, behavior: "smooth"});
@@ -410,23 +416,60 @@ function reset() {
     updateScore();
 }
 
-function solve() {
+function solve(size) {
     reset();
 
-    let circles = boardContainer.querySelectorAll(".board .circle");
-    [[0,7], [1,10], [2,13], [3,16], [4,11], [5,14], [6,17], [8,15], [9,18], [12,19]].forEach(pair => {
-        if(Math.random() >= 0.5) {
-            circles[pair[0]].click();
-        }else {
-            circles[pair[1]].click();
-        }
-    });
+    let solutions = {
+        "26-13": [1, 10, 15, 18, 26, 28, 35, 39, 50, 54, 63, 71, 73],
+        "30-15": [1, 4, 8, 12, 14, 18, 21, 22, 26, 28, 63, 66, 67, 71, 73],
+        "32-17": [1, 4, 11, 15, 17, 21, 26, 28, 38, 48, 49, 55, 59, 61, 65, 71, 74],
+        "33-17": [1, 4, 8, 9, 11, 24, 26, 27, 28, 38, 44, 47, 49, 55, 61, 66, 71],
+        "34-17": [1, 4, 11, 18, 21, 26, 32, 35, 40, 53, 55, 56, 59, 69, 71, 72, 75],
+        "36-19": [1, 4, 6, 12, 13, 18, 26, 29, 33, 42, 43, 46, 52, 53, 56, 59, 61, 71, 73],
+        "37-19": [1, 4, 6, 12, 17, 26, 35, 38, 40, 41, 44, 48, 55, 63, 65, 66, 71, 74, 75],
+        "38-19": [1, 2, 4, 5, 10, 26, 31, 34, 38, 40, 42, 44, 55, 62, 65, 69, 70, 71, 75],
+        "38-21": [1, 4, 6, 7, 9, 12, 19, 23, 25, 26, 29, 35, 37, 40, 41, 44, 48, 55, 66, 69, 74],
+        "39-21": [1, 4, 7, 11, 17, 19, 23, 26, 28, 29, 40, 42, 45, 47, 51, 53, 56, 63, 64, 67, 72],
+        "40-21": [1, 4, 14, 17, 18, 22, 26, 28, 30, 33, 35, 43, 46, 50, 53, 54, 55, 57, 59, 67, 71],
+        "41-21": [1, 10, 12, 13, 21, 24, 26, 31, 33, 34, 39, 40, 47, 49, 50, 55, 56, 58, 65, 67, 71],
+        "42-21": [1, 6, 7, 25, 26, 27, 28, 29, 30, 32, 34, 38, 47, 49, 53, 70, 71, 72, 73, 74, 75],
+        "43-23": [1, 2, 4, 11, 13, 15, 16, 24, 26, 27, 28, 35, 41, 44, 45, 50, 55, 61, 63, 65, 67, 68, 71],
+        "44-23": [1, 2, 4, 5, 8, 10, 12, 17, 18, 24, 25, 26, 28, 30, 39, 46, 50, 55, 59, 61, 66, 71, 74],
+        "45-23": [1, 4, 8, 10, 12, 13, 18, 20, 24, 26, 27, 28, 41, 46, 47, 52, 53, 55, 61, 63, 64, 65, 71],
+        "46-23": [1, 4, 11, 12, 13, 17, 21, 26, 27, 28, 31, 35, 44, 47, 50, 55, 57, 59, 61, 65, 70, 71, 75],
+        "48-25": [1, 3, 4, 14, 17, 24, 25, 26, 27, 28, 30, 31, 33, 34, 44, 46, 54, 55, 56, 57, 61, 69, 70, 71, 72],
+        "49-25": [1, 2, 6, 7, 14, 15, 16, 25, 26, 27, 28, 33, 41, 45, 48, 49, 51, 55, 56, 57, 59, 65, 69, 70, 71],
+        "50-25": [1, 3, 5, 10, 11, 16, 17, 21, 26, 27, 32, 33, 34, 40, 42, 47, 48, 49, 55, 57, 61, 62, 66, 71, 72],
+        "53-27": [1, 2, 6, 8, 10, 12, 13, 14, 15, 16, 17, 18, 22, 24, 26, 28, 30, 33, 42, 47, 55, 61, 62, 63, 71, 74, 75],
+        "54-27": [1, 3, 6, 7, 10, 11, 14, 16, 17, 18, 20, 21, 25, 26, 27, 31, 32, 33, 34, 35, 40, 41, 42, 48, 55, 61, 71],
+    }
 
-    let score = document.querySelectorAll(".scoreboard .entry");
-    for(let i = 0; i < 5; i++) {
-        if(score[i * 8].classList.contains("odd")){
-            circles[[20, 21, 22, 23, 24][i]].click();
-        }
+    let solution = solutions[size];
+
+    // the basis index order in the DOM is like
+    //  0   3   6   9  12
+    //  1   4   7  10  13
+    //  2   5   8  11  14
+    // ------------------
+    // 15  18 etc
+    // 16  19
+    // 17  20
+    // but the numbers above treat it like
+    //  0  15  30  45  60
+    //  1  16  31  46  61
+    //  2  17  32  47  62
+    // ------------------
+    //  3  18 etc
+    //  4  19
+    //  5  20
+
+    function convert_index(i){
+        var n = Math.floor((i % 15) / 3) * 15 + (i%3);
+        return n + Math.floor(i / 15) * 3;
+    }
+
+    for(var i in solution){
+        document.querySelectorAll(".basis")[convert_index(solution[i] - 1)].click();
     }
 
 }
