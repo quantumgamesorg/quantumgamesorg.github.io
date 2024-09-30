18101c3bdf0640df400fef1465bba5b8c4277e24
Mason Sakai <masonsakai@gmail.com>
2024-09-30 15:53:20 -0400

    tablegen 2-qubit

--- games/2-qubit-system.js
+++ games/2-qubit-system.js
@@ -63,27 +63,7 @@ document.querySelectorAll(".person").forEach((e, i) => {
 }
 //boardContainer.insertBefore(makeBoard(100), boardContainer.firstChild);
 
-
-// https://stackoverflow.com/a/950146
-function loadScript(url, callback)
-{
-    // Adding the script tag to the head as suggested before
-    var head = document.head;
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = url;
-
-    // Then bind the event to the callback function.
-    // There are several events for cross browser compatibility.
-    script.onreadystatechange = callback;
-    script.onload = callback;
-
-    // Fire the loading
-    head.appendChild(script);
-}
-
-loadScript("tablegen.js", function() {setupBoard();});
-
+setupBoard();
 
 
 
