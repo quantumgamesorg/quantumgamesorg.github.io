657e173ffdcc4531625db8536f259598141ebb86
Mason Sakai <masonsakai@gmail.com>
2025-02-05 11:18:46 -0500

    Updated PMSquare

--- games/PMSquare.js
+++ games/PMSquare.js
@@ -125,30 +125,6 @@ function generateRandomColor() {
 }
 
 window.onload = () => {
-    let content = document.getElementById("content");
-
-    let boardContainer = document.getElementById("boardContainer");
-    //const defaultBoardContainerSize = { width: parseInt(getComputedStyle(boardContainer).width), height: parseInt(getComputedStyle(boardContainer).height) };
-
-    document.getElementById("pauseButton").onclick = pauseSimulation;
-    document.querySelectorAll("#upperLeft button")[0].onclick = () => document.querySelector("#win_about").classList.toggle("hidden");
-    document.querySelectorAll("#upperLeft button")[1].onclick = () => window.scrollTo({top: document.querySelectorAll("#content > h2")[0].getBoundingClientRect().top + window.pageYOffset - 40, behavior: "smooth"});
-    // document.querySelectorAll("#uppoerLeft button")[2]
-    // document.querySelectorAll("#upperLeft button")[4].onclick = () => window.scrollTo({top: document.querySelectorAll("#content > h2")[1].getBoundingClientRect().top + window.pageYOffset - 40, behavior: "smooth"});
-    // document.querySelectorAll("#upperLeft button")[5].onclick = () => window.scrollTo({top: document.querySelectorAll("#content > h2")[2].getBoundingClientRect().top + window.pageYOffset - 40, behavior: "smooth"});
-    // document.querySelectorAll("#upperLeft button")[6].onclick = () => window.scrollTo({top: document.querySelectorAll("#content > h2")[3].getBoundingClientRect().top + window.pageYOffset - 40, behavior: "smooth"});
-    document.querySelectorAll("toTop").onclick = () => window.scrollTo({top: 0, behavior: "smooth"});
-
-    document.querySelectorAll(".person").forEach((e, i) => {
-        e.onmouseenter = ev => {
-            document.querySelectorAll(".personDesc")[Math.floor(i / 3)].classList.toggle("highlight", true);
-        };
-        e.onmouseleave = ev => {
-            document.querySelectorAll(".personDesc")[Math.floor(i / 3)].classList.toggle("highlight", false);
-        };
-    });
-
- 
     startSimulation();
 }
 
