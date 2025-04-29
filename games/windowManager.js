1e6d60475ca68fce57ea71a7b8785f0aace2da55
Mason Sakai <masonsakai@gmail.com>
2025-04-28 20:19:04 -0400

    Added more support for window names

--- games/windowManager.js
+++ games/windowManager.js
@@ -1,23 +1,27 @@
 
-var activeWindow = null
+var activeWindows = {}
 
 // check window.open for information
 // this just provides consistency and decent default settings
 function OpenWindow({url, name='subwindow', width=600, height=800}={}) {
 	try {
-		activeWindow = window.open(url, name, `width=${width}px, height=${height}px`)
-		if (activeWindow == null) {
+		activeWindows[name] = window.open(url, name, `width=${width}px, height=${height}px`)
+		if (activeWindows[name] == null) {
 			console.warn(`OpenWindow ${url} failed`)
 			return;
 		}
 		// resize corrections, add another 15 to width for scroll bar
 		width += 16 
 		height += 70
-		activeWindow.resizeTo(width, height)
+		activeWindows[name].resizeTo(width, height)
 	}
 	catch(e) {
 		console.error(e)
 	}
 }
 
-window.onbeforeunload = () => { if(activeWindow != null) activeWindow.close() }
+window.onbeforeunload = () => {
+	for (const key in activeWindows) {
+		activeWindows[key].close();
+	}
+}
